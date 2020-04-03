0526475494f8e3abfb32972199a8719089c48764
Jacob DeCourcey <jakedecourcey@gmail.com>
2020-04-03 22:48:58 +0100

    Added latency and auto-refresh

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,26 @@ function filterDataByTime(data, range){
     for (i = 0; i < data.length; i++) {
         timestamp = jsonData[i].timestamp.slice(0,-11).replace("T", " ");
         speed = (Math.round((Number(data[i].download)) / 1000 / 1000));
+        latency = Math.round(Number(data[i].ping));
         speedValues.push(speed);
-        appendToList(timestamp, speed);
+        appendToList(timestamp, latency, speed);
     }
     printAverageSpeed(speedValues);
 
     //for listing in data, delete if older than today - range
 }
 
-function appendToList(timestamp, speed){
-    d3.select("#data").append("p").text(timestamp + " | " + speed + " Mbps");
+function appendToList(timestamp, latency, speed){
+    d3.select("#data").append("p").text(timestamp + " | " + latency + " ms | " + speed + " Mbps");
 }
 
 function printAverageSpeed(values){
     d3.select("h2").text("Average: " + Math.round(values.reduce((a,b) => a + b, 0) / values.length) + " Mbps");
 }
 
+function main(){
+    setInterval(fetchData(), 60000);
+}
 
 function fetchData(time){
     var allData = new XMLHttpRequest();
