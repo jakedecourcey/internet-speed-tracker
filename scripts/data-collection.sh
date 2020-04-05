e0521297041a5f4d10b35ea1ca5a018adf5274eb
Jacob DeCourcey <jakedecourcey@gmail.com>
2020-04-05 19:04:57 +0100

    Moved installation to HOME

--- scripts/data-collection.sh
+++ scripts/data-collection.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+DATA=$HOME/internet-speed-tracker/data/data.json
+LOCK=$HOME/internet-speed-tracker/data/data.lock
+LOG=$HOME/internet-speed-tracker/scripts/error.log
+
 append_empty_data_entry(){
     # function to create an entry with empty fields when speedtest returns an error
-    echo '{"download": "", "upload": "", "ping": "", "server": {"url": "", "lat": "", "lon": "", "name": "", "country": "", "cc": "", "sponsor": "", "id": "", "host": "", "d": "", "latency": ""}, "timestamp": "$(date -u -Ins)", "bytes_sent": "", "bytes_received": "", "share": "", "client": {"ip": "", "lat": "", "lon": "", "isp": "", "isprating": "", "rating": "", "ispdlavg": "", "ispulavg": "", "loggedin": "", "country": ""}}]' >> /srv/share/internet-speed-tracker/data/data.lock
+    echo '{"download": "", "upload": "", "ping": "", "server": {"url": "", "lat": "", "lon": "", "name": "", "country": "", "cc": "", "sponsor": "", "id": "", "host": "", "d": "", "latency": ""}, "timestamp": "$(date -u -Ins)", "bytes_sent": "", "bytes_received": "", "share": "", "client": {"ip": "", "lat": "", "lon": "", "isp": "", "isprating": "", "rating": "", "ispdlavg": "", "ispulavg": "", "loggedin": "", "country": ""}}]' >> $LOCK
 }
 
 # Prevent script from intering with web request
-cp /srv/share/internet-speed-tracker/data/data.json /srv/share/internet-speed-tracker/data/data.lock
+cp $DATA $LOCK
 
 # Remove closing bracket and replace with comma to prepare for additional data
-sed -i '$ s/\]/,/' /srv/share/internet-speed-tracker/data/data.lock
+sed -i '$ s/\]/,/' $LOCK
 
 # Append speedtest data and close bracket
-speedtest --json >> /srv/share/internet-speed-tracker/data/data.lock 2>> /srv/share/internet-speed-tracker/scripts/error.log
+speedtest --json >> $LOCK 2>> $LOG
 if [ $? = 0 ]; then
     # close array with bracket
-    sed -i '$ s/$/]/' /srv/share/internet-speed-tracker/data/data.lock
+    sed -i '$ s/$/]/' $LOCK
 else
     # print empty data to file instead
     append_empty_data_entry
 fi
 
 # Put the new data in place to be requested
-mv /srv/share/internet-speed-tracker/data/data.lock /srv/share/internet-speed-tracker/data/data.json
+mv $LOCK $DATA
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@ mkdir /var/www/html/js /var/www/html/css /var/www/html/data
 
 files=("index.html" "js/scripts.js" "js/d3.js" "css/styles.css" "data/data.json")
 
-here="/srv/share/internet-speed-tracker"
-nginx="/var/www/html"
+here=$HOME/internet-speed-tracker
+nginx=/var/www/html
 
 for file in "${files[@]}";do
   ln -sf "${here}/${file}" "${nginx}/${file}"
